c4da4a0769488c56c0ad4be37189332518f9bffb
IhssanTsouli <ihssantsouli@gmail.com>
2023-01-20 11:41:42 +0100

    numbers

--- 5_Numbers/index.php
+++ 5_Numbers/index.php
@@ -14,6 +14,37 @@ var_dump(is_int($x));
 
 $x = 59.85;
 var_dump(is_int($x));
+
+$x = 10.365;
+var_dump(is_float($x));
+
+$x = 1.9e411;
+var_dump($x);
+
+//PHP Numerical Strings
+$x = 5985;
+var_dump(is_numeric($x)); //bool(true)
+
+$x = "5985";
+var_dump(is_numeric($x)); //bool(true)
+
+$x = "59.85" + 100;
+var_dump(is_numeric($x)); //bool(true)
+
+$x = "Hello";
+var_dump(is_numeric($x)); //bool(false)
+
+// Cast float to int
+$x = 23465.768;
+$int_cast = (int)$x; //23465
+echo $int_cast;
+
+echo "<br>";
+
+// Cast string to int
+$x = "23465.768";
+$int_cast = (int)$x; //23465
+echo $int_cast;
 ?>
 </body>
 </html>
